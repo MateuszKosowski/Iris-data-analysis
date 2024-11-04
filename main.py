813477f7472e2442257b9fb229b8fc330dcd294f
MateuszKosowski <mateuszkosowski03@gmail.com>
2024-11-04 14:14:09 +0100

    Add boxplot - work fine

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ file = './Zadanie 1 Dane-20241103/data1.csv'
 data = pd.read_csv(file, header=None, sep=',')
 data.columns = ['Dlugosc kielicha', 'Szerokosc kielicha', 'Dlugosc platka', 'Szerokosc platka', 'Gatunek']
 
+
+# ------------------- Zadanie 1 -------------------
+
+
 # Konwersja DataFrame do listy list (tablicy dwuwymiarowej)
 dataList = data.values.tolist()
 
@@ -34,37 +38,35 @@ def countSpecies(dataList):
 # Wywołanie funkcji countSpecies
 countSpecies(dataList)
 
-def charakterystykaCechy(data, name):
-    nazwa = data[name]
-    print(f'\nNazwa: {name}')
-    print(f'Minimalna: {min(nazwa)}')
-    print(f'Srednia: {np.mean(nazwa)}')
-    print(f'Odchylenie standardowe: {np.std(nazwa)}')
-    print(f'Mediana: {np.median(nazwa)}')
-    print(f'Kwartyl dolny: {np.percentile(nazwa, 25)}')
-    print(f'Kwartyl gorny: {np.percentile(nazwa, 75)}')
-    print(f'Maksymalna: {max(nazwa)}')
+# Statystyki dla danej cechy
+def featureStatistics(data):
+    print(f'\nNazwa: {data.name}')
+    print(f'Minimalna: {min(data)}')
+    print(f'Srednia: {np.mean(data)}')
+    print(f'Odchylenie standardowe: {np.std(data)}')
+    print(f'Mediana: {np.median(data)}')
+    print(f'Kwartyl dolny: {np.percentile(data, 25)}')
+    print(f'Kwartyl gorny: {np.percentile(data, 75)}')
+    print(f'Maksymalna: {max(data)}')
 
-# Charakterystyka cech dla dlugosci kielicha
-charakterystykaCechy(data, 'Dlugosc kielicha')
 
-# Charakterystyka cech dla szerokosci kielicha
-charakterystykaCechy(data, 'Szerokosc kielicha')
+# Wywołanie funkcji featureStatistics dla każdej cechy
+featureStatistics(data['Dlugosc kielicha'])
+featureStatistics(data['Szerokosc kielicha'])
+featureStatistics(data['Dlugosc platka'])
+featureStatistics(data['Szerokosc platka'] )
 
-# Charakterystyka cech dla dlugosci platka
-charakterystykaCechy(data, 'Dlugosc platka')
 
-# Charakterystyka cech dla szerokosci platka
-charakterystykaCechy(data, 'Szerokosc platka')
+# ------------------- Zadanie 2 -------------------
 
-# Histogram dla dlugosci kielicha
+# fig - obiekt typu Figure, axs - tablica obiektów typu Axes, 4 wiersze, 2 kolumny, figsize - rozmiar wykresu w calach
+fig, axs = plt.subplots(4, 2, figsize=(12, 17))
 
-# Funkcja histogram
-# data - dane, name - nazwa kolumny, bins - ilosc przedzialow, edgecolor - kolor krawedzi, range - zakres osi x
-
-fig, axs = plt.subplots(4, 2, figsize=(12, 15))
-
+# Wykresy histogramów
 def histogram(x, y, data, bins, edgecolor, range, title, ylim):
+
+    # axs[x][y] - obiekt typu Axes, na którym rysowany jest histogram
+    # data - dane, bins - liczba przedziałów, edgecolor - kolor krawędzi, range - zakres danych, title - tytuł wykresu, ylim - zakres osi y
     axs[x][y].hist(data, bins=bins, edgecolor=edgecolor, range=range)
 
     # Opis osi x i y oraz tytul
@@ -75,23 +77,41 @@ def histogram(x, y, data, bins, edgecolor, range, title, ylim):
     # Ustawienie zakresu osi y
     axs[x][y].set_ylim(ylim)
 
-def boxplot(x, y, data, ylim):
-    axs[x][y].boxplot(data)
 
-    # Opis osi x i y oraz tytul
-    axs[x][y].set_ylabel('Długość (cm)')
-
-    # Ustawienie zakresu osi y
-    axs[x][y].set_ylim(ylim)
-
-# Wywołanie funkcji histogram dla dlugosci kielicha
+# Wywołanie funkcji histogram dla każdej cechy
 histogram(0, 0, data['Dlugosc kielicha'], 8, 'black', (4.0, 8.0), 'Długość działki kielicha', (0, 35))
 histogram(1, 0, data['Szerokosc kielicha'], 5, 'black', (2.0, 4.5), 'Szerokość działki kielicha', (0, 75))
-histogram(2, 0, data['Dlugosc platka'], 12, 'black', (1.0, 7.0), 'Szerokość działki kielicha', (0, 30))
-histogram(3, 0, data['Szerokosc platka'], 5, 'black', (0.0, 2.5), 'Szerokość działki kielicha', (0, 55))
+histogram(2, 0, data['Dlugosc platka'], 12, 'black', (1.0, 7.0), 'Długość płatka', (0, 30))
+histogram(3, 0, data['Szerokosc platka'], 5, 'black', (0.0, 2.5), 'Szerokość płatka', (0, 55))
 
-boxplot(1, 0, data['Dlugosc kielicha'], (4, 8))
 
+# Przypisanie wartości numerycznych do nazw gatunków
+# spacies_labels - słownik przypisujący wartości numeryczne do nazw gatunków
+# mapowanie wartości numerycznych na nazwy gatunków - zamiana wartości numerycznych na nazwy gatunków w kolumnie 'Gatunek' w obiekcie data
+species_labels = {0: 'Setosa', 1: 'Versicolor', 2: 'Virginica'}
+data['Gatunek'] = data['Gatunek'].map(species_labels)
+
+# Generowanie boxplotów
+def boxplot_by_species(x, y, feature, ylabel, ylim):
+
+    # data.boxplot - generowanie boxplotów, column - cecha, by - grupowanie po gatunku, ax - obiekt typu Axes, na którym rysowany jest boxplot
+    data.boxplot(column=feature, by='Gatunek', ax=axs[x][y])
+    axs[x][y].set_title('')
+    axs[x][y].set_ylim(ylim)
+    axs[x][y].set_xlabel('Gatunek')
+    axs[x][y].set_ylabel(ylabel)
+    axs[x][y].grid(False)
+
+# Generowanie boxplotów dla każdej cechy
+boxplot_by_species(0,1, 'Dlugosc kielicha', 'Długość (cm)', (4, 8))
+boxplot_by_species(1,1, 'Szerokosc kielicha', 'Szerokość (cm)', (0, 6))
+boxplot_by_species(2,1, 'Dlugosc platka', 'Długość (cm)', (0,8))
+boxplot_by_species(3,1, 'Szerokosc platka', 'Szerokość (cm)', (0, 4))
+
+plt.suptitle('Analiza Irysów')
 plt.subplots_adjust(wspace=0.2, hspace=0.6)
 
 plt.show()
+
+
+
